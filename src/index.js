306cf66b05eac6bf7e1ab95cabdb6f6d0e4c70d7
Bismarck <bismarck@email.com>
2022-12-09 10:07:16 -0300

    fix: init project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import express, { json } from "express";
+import cors from "cors";
+const app = express();
+app.use(json());
+app.use(cors());
+
+app.listen(5000, () => console.log("Conectado na porta 5000"));
