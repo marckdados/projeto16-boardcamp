536c5f8261a740b3505d4f0ce74a11d5c0b935f3
Bismarck <bismarckdados@gmail.com>
2022-12-11 16:23:12 -0300

    feat: get customers per id and update customers

--- src/controllers/CustomersController.js
+++ src/controllers/CustomersController.js
@@ -1,13 +1,56 @@
 import connectionDB from "../server.js";
 export async function createCustomer(req, res) {
   const { name, phone, cpf, birthday } = res.locals.customers;
-
   try {
     await connectionDB.query(
       "INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1,$2,$3,$4)",
       [name, phone, cpf, birthday]
     );
 
+    res.sendStatus(201);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+}
+
+export async function getCustomers(req, res) {
+  try {
+    const customers = await connectionDB.query("SELECT * FROM customers");
+    res.send(customers.rows);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+}
+
+export async function getCustomersById(req, res) {
+  const { id } = req.params;
+  console.log(id);
+  try {
+    const idExists = await connectionDB.query(
+      "SELECT * FROM customers WHERE id=$1;",
+      [id]
+    );
+    if (!idExists.rows[0]) {
+      return res.sendStatus(404);
+    }
+    res.send(idExists.rows[0]);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+}
+
+export async function updateCustomers(req, res) {
+  const { id } = req.params;
+  const { name, phone, cpf, birthday } = res.locals.customers;
+
+  try {
+    await connectionDB.query(
+      "UPDATE customers SET name=$1, phone=$2, cpf=$3, birthday=$4 WHERE id=$5;",
+      [name, phone, cpf, birthday, id]
+    );
     res.sendStatus(201);
   } catch (error) {
     console.log(error);
--- src/routers/CustomersRouter.js
+++ src/routers/CustomersRouter.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
-import { createCustomer } from "../controllers/CustomersController.js";
+import {
+  createCustomer,
+  getCustomers,
+  getCustomersById,
+  updateCustomers,
+} from "../controllers/CustomersController.js";
 import { customersValidate } from "../middlewares/CustomersValidationMiddleware.js";
 
 const customersRouter = Router();
 
 customersRouter.post("/customers", customersValidate, createCustomer);
+customersRouter.get("/customers", getCustomers);
+customersRouter.get("/customers/:id", getCustomersById);
+customersRouter.put("/customers/:id", customersValidate, updateCustomers);
 
 export default customersRouter;
